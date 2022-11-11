0ccbc1f889d3befa4ec2e9e4084a9d29dc197ee0
Apexmo <73833180+Apexmo@users.noreply.github.com>
2022-11-11 17:28:41 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,8 @@
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
+#ifndef MAIN_H
+#define MAIN_H
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+unsigned int _strlen(char *s);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif
